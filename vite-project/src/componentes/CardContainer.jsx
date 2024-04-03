ebbdcdc4ef1cf53625ec713e272f48e72a15273f
thomas cicchetti <thomascic99@gmail.com>
2024-04-03 00:18:22 -0300

    Sumar carrito y cards styles

--- vite-project/src/componentes/CardContainer.jsx
+++ vite-project/src/componentes/CardContainer.jsx
@@ -5,9 +5,9 @@ import '../estilos/cardContainerStyle.css'
 function CardContainer() {
   return (
     <div className='structureContainer'>
-        <ItemListContainer nombre="Pantalón" precio="$25.000"/>
-        <ItemListContainer nombre="Remera" precio="$19.000$"/>
-        <ItemListContainer nombre="Camisa" precio="$29.000"/>
+        <ItemListContainer nombre="Oreja de Elefante" precio="$5.000"/>
+        <ItemListContainer nombre="Begonia" precio="$8.000"/>
+        <ItemListContainer nombre="Paleta de Pintor" precio="$2.500"/>
     </div>
   )
 }
